89440914376cbd1a4d6868337f4d86b460571cd3
Litong Deng <litong.deng@compass.com>
2022-02-05 12:29:00 +0800

    Add AutoValue basic generated class sample
    Signed-off-by: Litong Deng <litong.deng@compass.com>

--- README.md
+++ README.md
@@ -1,8 +1,18 @@
 # java-auto-value-maven-demo
 
+## User guide
+
+https://github.com/google/auto/blob/master/value/userguide/index.md
+
+## Why use AutoValue
+
+https://github.com/google/auto/blob/master/value/userguide/why.md
+
 ## Background
 
-AutoValue concept: You write an abstract class, and AutoValue implements it.
+- AutoValue concept: You write an abstract class, and AutoValue implements it.
+- Be careful that you don't accidentally pass parameters to the generated constructor in the wrong
+  order. Consider switching to use the builder option to avoid this problem.
 
 ## Usage
 
@@ -11,7 +21,3 @@ See the unit tests.
 ```
 mvn clean package test
 ```
-
-## Github repo user guide
-
-https://github.com/google/auto/blob/master/value/userguide/index.md
---
+++ src/main/java/com/denglitong/AutoValue_Animal_Generated.java
@@ -0,0 +1,53 @@
+/**
+ * Copyright (C) 2022 Urban Compass, Inc.
+ */
+package com.denglitong;
+
+import java.util.Objects;
+
+import javax.annotation.processing.Generated;
+
+@Generated("com.google.auto.value.processor.AutoValueProcessor")
+final class AutoValue_Animal_Generated extends Animal {
+  private final String name;
+  private final int numberOfLegs;
+
+  public AutoValue_Animal_Generated(String name, int numberOfLegs) {
+    if (name == null) {
+      throw new NullPointerException("Null name");
+    }
+    this.name = name;
+    this.numberOfLegs = numberOfLegs;
+  }
+
+  @Override
+  String name() {
+    return name;
+  }
+
+  @Override
+  int numberOfLegs() {
+    return numberOfLegs;
+  }
+
+  @Override
+  public String toString() {
+    return "AutoValue_Animal_Generated{" +
+        "name='" + name + '\'' +
+        ", numberOfLegs=" + numberOfLegs +
+        '}';
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {return true;}
+    if (o == null || getClass() != o.getClass()) {return false;}
+    AutoValue_Animal_Generated that = (AutoValue_Animal_Generated)o;
+    return numberOfLegs == that.numberOfLegs && Objects.equals(name, that.name);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(name, numberOfLegs);
+  }
+}
